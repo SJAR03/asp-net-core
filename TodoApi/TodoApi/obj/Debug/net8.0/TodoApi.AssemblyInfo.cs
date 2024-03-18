530384965778adcfd637476ee1d716ecd225f046
SJAR03 <sayerdisrodriguez@gmail.com>
2024-03-17 23:34:06 -0600

    crud

--- TodoApi/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+356db4ae4fbd1970efc997836260a57969bf2f80")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
